42e31d9e5a7b15657fa771fbfbd2125a19c5beee
Natalie Zavada <3littlebirds414@gmail.com>
2019-05-01 15:43:30 -0500

    Initial Commit

---
+++ SuperCats/Models/SuperCat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperCats.Models
+{
+    public class SuperCat
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Temperment { get; set; }
+        public string Sound { get; set; }
+        public string CoatColor { get; set; }
+    }
+}
